f47459674a64ba62e5ecdbaf53abf716f5277408
xaviguasch <xaviguasch@gmail.com>
2020-08-08 15:41:41 +0200

    captured user's search and matched it with the user named

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('hey')
+let searchInput = document.getElementById('searchInput')
+const names = document.querySelectorAll('.name')
+const results = document.getElementById('results')
+
+const arr = []
+let searchQuery = ''
+
+for (const name of names) {
+  arr.push(name.innerText)
+}
+
+searchInput.addEventListener('keyup', (e) => {
+  searchQuery = e.target.value.toLowerCase()
+})
+
+searchInput.addEventListener('click', (e) => {
+  arr.forEach((name) => {
+    if (name.toLowerCase() === searchQuery) {
+      console.log('a match')
+      const match = (document.createElement('h2').innerText = name)
+      results.append(match)
+    }
+  })
+})
